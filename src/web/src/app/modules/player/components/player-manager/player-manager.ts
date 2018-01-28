d14975ed89f60c19ed1bb050f0bf034e0929cf98
zarlex <alex.zarges@online.de>
2018-01-28 19:46:54 +0100

    Handle fade for tracks that are shorter than the actual fade duration

--- src/web/src/app/modules/player/components/player-manager/player-manager.ts
+++ src/web/src/app/modules/player/components/player-manager/player-manager.ts
@@ -103,6 +103,30 @@ export class PlayerManagerComponent implements OnInit, OnChanges {
     this._playerSubscriptions = new Subscription();
   }
 
+  private getFadeDuration() {
+    if (this._activePlayer && this._upcomingPlayer) {
+      const currentPlayerDuration = this._activePlayer.instance.getDuration();
+      const upcomingPlayerDuration = this._upcomingPlayer.instance.getDuration();
+      const maxFadeDuration = Math.min(currentPlayerDuration / 2, upcomingPlayerDuration / 2);
+      const minFadeDuration = Math.min(this._fadeDuration, maxFadeDuration);
+      if (maxFadeDuration < minFadeDuration) {
+        return maxFadeDuration;
+      } else {
+        return minFadeDuration;
+      }
+    } else {
+      return 0;
+    }
+  }
+
+  private getFadeStartTime() {
+    if (this._activePlayer && this._activePlayer.instance.getDuration() > 0) {
+      return this._activePlayer.instance.getDuration() - this.getFadeDuration();
+    } else {
+      return false;
+    }
+  }
+
   private bindListeners(player: IPlayer): void {
     this.unBindListeners();
 
@@ -127,11 +151,8 @@ export class PlayerManagerComponent implements OnInit, OnChanges {
     const crossFadeSubscription = this._playerSubscriptions.add(
       player.currentTimeChange
         .filter(currentTime => {
-          if (player.getDuration() > this._fadeDuration) {
-            return currentTime >= player.getDuration() - this._fadeDuration;
-          } else {
-            return false;
-          }
+          const fadeStartTime = this.getFadeStartTime();
+          return fadeStartTime && currentTime >= fadeStartTime;
         })
         .subscribe(() => {
           crossFadeSubscription.unsubscribe();
@@ -168,9 +189,11 @@ export class PlayerManagerComponent implements OnInit, OnChanges {
       nextPlayer.instance.play().then(() => {
         nextPlayer.instance.setSize(PlayerFactory.getPlayerSize(nextPlayer.instance.track));
         nextPlayer.instance.setVolume(this._volume);
-        nextPlayer.instance.fadeIn(this._fadeDuration * 1000);
-        currentPlayer.instance.fadeOut(this._fadeDuration * 1000);
-        EaseService.easeInCirc(0, 1, (this._fadeDuration - 1) * 1000)
+
+        const fadeDuration = this.getFadeDuration();
+        nextPlayer.instance.fadeIn(fadeDuration * 1000);
+        currentPlayer.instance.fadeOut(fadeDuration * 1000);
+        EaseService.easeInCirc(0, 1, (fadeDuration - 1) * 1000)
           .subscribe((value: number) => {
             nextPlayer.instance.setOpacity(value);
           });
