deb68bc682d2ff8a84ba12d31ef7ea96f98cdfc5
zarlex <alex.zarges@online.de>
2018-01-29 11:13:06 +0100

    Make observable

--- src/web/src/app/modules/shared/services/window-messages.service.ts
+++ src/web/src/app/modules/shared/services/window-messages.service.ts
@@ -1,9 +1,20 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {isObject, isString} from 'underscore';
 import {IMessage, MessageMethodTypes, MessageService, MessageSubscription, MessageTypes} from './message.service';
 
+export enum WindowMessageStatusTypes {
+  MESSAGE = 'MESSAGE'
+}
+
+export interface IWindowMessageEvent {
+  type: WindowMessageStatusTypes;
+  detail?: any;
+}
+
 @Injectable()
 export class WindowMessagesService {
+  private _observable: EventEmitter<IWindowMessageEvent>;
+
   public static emitAppEvent(message: IMessage) {
     const ev = new CustomEvent('appMessage', {
       detail: JSON.stringify(message)
@@ -13,6 +24,7 @@ export class WindowMessagesService {
 
   constructor(private messageService: MessageService) {
     window.addEventListener('appMessage', this.onMessage.bind(this));
+    this._observable = new EventEmitter();
   }
 
   private onMessage(event: CustomEvent) {
@@ -27,6 +39,7 @@ export class WindowMessagesService {
       method: MessageMethodTypes[jsonData.method as keyof typeof MessageMethodTypes],
       body: jsonData.body
     });
+    this._observable.emit({type: WindowMessageStatusTypes.MESSAGE, detail: jsonData});
   }
 
   public sendMessage(channelId: string, method: MessageMethodTypes, body: any) {
@@ -40,5 +53,9 @@ export class WindowMessagesService {
 
   public unSubscribe(channelId: string, method?: MessageMethodTypes, callback?: Function) {
     return this.messageService.unSubscribe(MessageTypes.WINDOW_MESSAGE, channelId, method, callback);
+  }
+
+  public getObservable() {
+    return this._observable;
   }
 }
