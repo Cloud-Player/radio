fd07aa05f844724a752dcae8eaf2372d32977559
Nicolas Drebenstedt <cutoffthetop@me.com>
2018-01-30 12:08:19 +0100

    Add component event tests

--- src/cloudplayer/tests/conftest.py
+++ src/cloudplayer/tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 import cloudplayer.iokit.gpio
+import cloudplayer.iokit.event
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -8,3 +9,11 @@ def mock_gpio(monkeypatch):
     mock_gpio = cloudplayer.iokit.gpio.MockGPIO()
     monkeypatch.setattr(cloudplayer.iokit.gpio.GPIO, 'gpio', mock_gpio)
     return mock_gpio
+
+
+@pytest.fixture(scope='function', autouse=True)
+def event_manager():
+    em = cloudplayer.iokit.event.EventManager
+    yield em
+    em.queue._queue.clear()
+    em.subscriptions.clear()
---
+++ src/cloudplayer/tests/test_component.py
@@ -0,0 +1,34 @@
+import mock
+from cloudplayer.iokit import Component
+from cloudplayer.iokit import EventManager
+
+
+def test_component_should_provide_uuid():
+    assert len(Component().uuid) == 32
+
+
+def test_component_should_publish_event_to_manager():
+    comp = Component()
+    comp.publish('ACTION', 42)
+    event = EventManager.queue.get_nowait()
+    assert event.action == 'ACTION'
+    assert event.value == 42
+    assert event.publisher == comp
+
+
+def test_component_should_subscribe_to_event():
+    c1 = Component()
+    c2 = Component()
+    c1.subscribe('ACTION', c2)
+    trigger = 'ACTION@{}'.format(c1.uuid)
+    assert c2 in EventManager.subscriptions.get(trigger)
+
+
+def test_component_should_unsubscribe_from_event():
+    c1 = Component()
+    c2 = Component()
+    c1.subscribe('ACTION', c2)
+    trigger = 'ACTION@{}'.format(c1.uuid)
+    assert c2 in EventManager.subscriptions.get(trigger)
+    c1.unsubscribe('ACTION', c2)
+    assert c2 not in EventManager.subscriptions.get(trigger)
